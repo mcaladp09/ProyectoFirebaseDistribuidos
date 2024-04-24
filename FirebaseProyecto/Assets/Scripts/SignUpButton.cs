a8c4c8f761323eb25aafd5f7dc9bc66255f66067
mcaladp09 <theredstonefighter@gmail.com>
2024-04-23 22:19:26 -0500

    Config

---
+++ FirebaseProyecto/Assets/Scripts/SignUpButton.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Firebase.Auth;
+
+public class SignUpButton : MonoBehaviour
+{
+    [SerializeField]
+    private Button _signupButton;
+    [SerializeField]
+    private TMP_InputField _emailInputField;
+    [SerializeField]
+    private TMP_InputField _passwordInputField;
+
+    private void Reset()
+    {
+        _signupButton = GetComponent<Button>();
+        _emailInputField = GameObject.Find("InputFieldEmail").GetComponent<TMP_InputField>();
+        _passwordInputField = GameObject.Find("InputFieldPassword").GetComponent<TMP_InputField>();
+    }
+
+    void Start()
+    {
+        _signupButton.onClick.AddListener(HandleSignupButtonClicked);
+    }
+
+    private void HandleSignupButtonClicked() 
+    {
+        StartCoroutine(RegisterUser());
+    }
+
+    private IEnumerator RegisterUser() 
+    {
+        var auth = FirebaseAuth.DefaultInstance;
+
+        var signuptask = auth.CreateUserWithEmailAndPasswordAsync(_emailInputField.text, _passwordInputField.text);
+
+        yield return new WaitUntil(() => signuptask.IsCompleted);
+
+        if (signuptask.IsCanceled)
+        {
+            Debug.LogError("CreateUserWithEmailAndPasswordAsync was canceled.");
+        }
+
+        else if (signuptask.IsFaulted)
+        {
+            Debug.LogError("CreateUserWithEmailAndPasswordAsync encountered an error: " + signuptask.Exception);
+        }
+
+        else 
+        {
+            // Firebase user has been created.
+            Firebase.Auth.AuthResult result = signuptask.Result;
+            Debug.LogFormat("Firebase user created successfully: {0} ({1})",
+                result.User.DisplayName, result.User.UserId);
+        }
+    }
+}
